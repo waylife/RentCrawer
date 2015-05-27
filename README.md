4ded228808e7e1881434fdae2c70c0088c3bc8c6
WangYun <waylife365@gmail.com>
2015-05-27 21:39:13 +0800

    mod 增加文档细节 add 增加搜索的豆瓣小组数 fix 豆瓣小组无法搜索问题 fix windows下显示乱码问题

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Rent Crawer 租房爬虫
+# Rent Crawler 租房爬虫
 This is a house rental information crawler project for people living in Beijing.
-Currently no other language documents are available.  
+Currently no other language documents are available, Chinese documents only.
 
 ------
 
@@ -14,12 +14,29 @@ Currently no other language documents are available.
 > * 可以配置帖子开始时间
 
 抓取结果示意图：
-![Rent Crawer Result](https://github.com/waylife/RentCrawer/blob/master/Images/result_1.0.png?raw=true)
+![Rent Crawelr Result](https://github.com/waylife/RentCrawer/blob/master/Images/result_1.0.png?raw=true)
 
 ##使用说明
-1. 【必须】配置RentCrawler.py中52行key_search_word字段为你先搜索的区域
-2. 【必须】配置RentCrawler.py中53行custom_black_list字段为你不想搜到的内容
-3. 【必须】配置RentCrawler.py中54行start_time_str字段为帖子开始时间
+需要先安装Requests以及BeautifulSoup库，如果已经安装可以忽略，没有安装，使用以下命令安装：
+
+``` bash
+pip install requests
+pip install beautifulsoup4
+```
+
+1. 【必须】配置RentCrawler.py中54行key_search_word字段为你先搜索的区域
+2. 【必须】配置RentCrawler.py中55行custom_black_list字段为你不想搜到的内容
+3. 【必须】配置RentCrawler.py中56行start_time_str字段为帖子开始时间
 4. 【可选】删除rentdata.db以及result.html文件，如果不希望本次结果与上次混合在一起，可以删除相关文件
 5. 【必须】运行RentCrawler.py文件
-6. 【必须】程序结束后打开result.html查看结果
+6. 【必须】等待程序结束后打开result.html查看结果
+
+##注意
+由于豆瓣有反刷机制，每天运行次数不要过多，有可能导致当天无法获取数据，也就是引发403错误
+豆瓣数据请求目前为每获得一次结果暂停1s
+
+##反馈与建议
+可通过https://github.com/waylife/RentCrawer/issues/new提交
+
+##改进
+欢迎各路大神提供改进意见
--- RentCrawler/RentCrawler.py
+++ RentCrawler/RentCrawler.py
@@ -4,7 +4,9 @@ __author__ = 'rxread'
 import sqlite3
 import re
 import sys
+import time
 import datetime
+import webbrowser
 
 import requests
 from bs4 import BeautifulSoup
@@ -43,20 +45,19 @@ Beijing, China
 # set encoding
 reload(sys)
 sys.setdefaultencoding('utf8')
-prog_info = "Rent Crawler 1.0\nBy RxRead\nhttp://blog.zanlabs.com\n"
+prog_info = "Rent Crawler 1.1\nBy RxRead\nhttp://blog.zanlabs.com\n"
 print prog_info
 
 
 #######################################
 #You can modify configurations below
-key_search_word = ('三元桥', '北京西站', '国贸')
-custom_black_list = ('隔断', '单间','一居室')
-
+key_search_word = (u'三元桥',u'国贸',u'回龙观')
+custom_black_list = (u'隔断', u'单间', u'一居室')
 start_time_str = '2015-03-12'
 #You can modify configurations above
 ######################################
 
-smth_black_list = ('黑名单', 'Re', '警告', '发布', '关于', '通知', '审核', '求助', '规定', '求租')
+smth_black_list = (u'黑名单', u'Re', u'警告', u'发布', u'关于', u'通知', u'审核', u'求助', u'规定', u'求租')
 
 newsmth_headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.65 Safari/537.36',
@@ -65,6 +66,17 @@ newsmth_headers = {
     'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4,en-GB;q=0.2,zh-TW;q=0.2',
     'Connection': 'keep-alive',
     'X-Requested-With': 'XMLHttpRequest'  #important parameter,can not ignore
+}
+
+douban_headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, sdch',
+    'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4,en-GB;q=0.2,zh-TW;q=0.2',
+    'Connection': 'keep-alive',
+    'DNT': '1',
+    'HOST': 'www.douban.com',
+    'Cookie': 'bid="aszzdddsw"; ue="usyhhs@qq.com"; ll="10822388"; _pk_ref.100001.8cb4=%5B%22%22%2C%22%22%2C1432697511%2C%22https%3A%2F%2Fwww.baidu.com%2Flink%3Furl%3D7gqnBpvJdRVNXiY3uz6NC8MakeEiO9sJ5MkSPCVt1_5XtHvTf9hnUt8ZpUIThjOz%26wd%3D%25E8%25B1%2586%25E7%2593%25A3%25E5%258C%2597%25E4%25BA%25AC%25E7%25A7%259F%25E6%2588%25BF%2520%25E5%25B8%2596%25E5%25AD%2590%2520%25E8%2587%25AA%25E5%258A%25A8%25E6%258A%2593%25E5%258F%2596%26issp%3D1%26f%3D8%26ie%3Dutf-8%26tn%3Dbaiduhome_pg%26inputT%3D5816%22%5D; ps=y; __utmt=1; dbcl2="68564289:Us+tdIIhfuI"; ck="oNkx"; ap=1; push_noty_num=0; push_doumail_num=0; _pk_id.100001.8cb4=c8a8b67bc01d90d4.1428030074.12.1432699611.1432695397.; _pk_ses.100001.8cb4=*; __utma=20149791.251340141.1428030075.14321694682.1432697511.13; __utmb=20149791.26.10.1432697511; __utmc=20149791; __utmz=20149791.1432535587.8.5.utmcsr=baidu|utmccn=(organic)|utmcmd=organic|utmctr=%E8%B1%86%E7%93%A3%E5%8C%97%E4%BA%AC%E7%A7%9F%E6%88%BF%20%E5%B8%96%E5%AD%90%20%E8%87%AA%E5%8A%A8%E6%8A%93%E5%8F%96; __utmv=20149791.6856'
 }
 
 smth_switch=True;
@@ -78,7 +90,7 @@ try:
     conn.text_factory=str
     cursor = conn.cursor()
     cursor.execute(
-        'CREATE TABLE IF NOT EXISTS rent(id INTEGER PRIMARY KEY, title TEXT, url TEXT UNIQUE,itemtime timestamp, crawtime timestamp ,author TEXT, source TEXT,note TEXT)')
+        'CREATE TABLE IF NOT EXISTS rent(id INTEGER PRIMARY KEY, title TEXT, url TEXT UNIQUE,itemtime timestamp, crawtime timestamp ,author TEXT, source TEXT,keyword TEXT,note TEXT)')
     cursor.close()
     start_time = getTimeFromStr(start_time_str)
     print "searching data after date ", start_time
@@ -118,33 +130,44 @@ try:
                             author_text = tr.find_all(attrs={'class': 'title_12'})[0].find_all('a')[0].text
                             try:
                                 cursor.execute(
-                                    'INSERT INTO rent(id,title,url,itemtime,crawtime,author,source,note) VALUES(NULL,?,?,?,?,?,?,?)',
-                                    [title_text, link_text, getTimeFromStr(time_text), datetime.datetime.now(), author_text,
+                                    'INSERT INTO rent(id,title,url,itemtime,crawtime,author,source,keyword,note) VALUES(NULL,?,?,?,?,?,?,?,?)',
+                                    [title_text, link_text, getTimeFromStr(time_text), datetime.datetime.now(), author_text,keyword,
                                      'newsmth', ''])
-                                print 'add new data:', title_text, time_text, author_text, link_text
+                                print 'add new data:', title_text, time_text, author_text, link_text,keyword
                                 #/nForum/article/HouseRent/225839 /nForum/#!article/HouseRent/225839
                             except sqlite3.Error, e:
                                 print 'data exists:',title_text,link_text,e
-                                pass
                     except Exception, e:
                         print "error match table", e
                 else:
                     print "no data"
             else:
-                print 'request url error:' + url
+                print 'request url error %s -status code: %s:' %(url,r.status_code)
     #end newsmth
 
     #Douban: Beijing Rent,Beijing Rent Douban
     if douban_siwtch:
         douban_url = ('http://www.douban.com/group/search?group=35417&cat=1013&q=',
-                      'http://www.douban.com/group/search?group=26926&cat=1013&q=')
-        douban_url_name=('Douban-北京租房','Douban-北京租房豆瓣')
+                      'http://www.douban.com/group/search?group=26926&cat=1013&q=',
+                      'http://www.douban.com/group/search?group=262626&cat=1013&q=',
+                      'http://www.douban.com/group/search?group=252218&cat=1013&q=',
+                      'http://www.douban.com/group/search?group=279962&cat=1013&q=',
+                      'http://www.douban.com/group/search?group=257523&cat=1013&q=',
+                      'http://www.douban.com/group/search?group=232413&cat=1013&q=',
+                      'http://www.douban.com/group/search?group=135042&cat=1013&q=',
+                      'http://www.douban.com/group/search?group=252091&cat=1013&q=',
+                      'http://www.douban.com/group/search?group=10479&cat=1013&q=',
+                      'http://www.douban.com/group/search?group=221207&cat=1013&q=')
+        douban_url_name=(u'Douban-北京租房',u'Douban-北京租房豆瓣',u'Douban-北京无中介租房',
+                         u'Douban-北京租房专家',u'Douban-北京租房（非中介）',u'Douban-北京租房房东联盟(中介勿扰) ',
+                         u'Douban-北京租房（密探）',u'Douban-北漂爱合租（租房）',u'Douban-豆瓣♥北京♥租房',
+                         u'Douban-吃喝玩乐在北京',u'Douban-北京CBD租房')
         douban_url_index=0
         for url in douban_url:
             for keyword in key_search_word:
                 print '>>>>>>>>>>Search %s  %s ...' % (douban_url_name[douban_url_index],keyword)
-                url = url + keyword
-                r = requests.get(url, headers=newsmth_headers)
+                url_link = url + keyword
+                r = requests.get(url_link, headers=douban_headers)
                 if r.status_code == 200:
                     try:
                         soup = BeautifulSoup(r.text)
@@ -170,17 +193,17 @@ try:
                             reply_count=td[2].find_all('span')[0].text
                             try:
                                 cursor.execute(
-                                    'INSERT INTO rent(id,title,url,itemtime,crawtime,author,source,note) VALUES(NULL,?,?,?,?,?,?,?)',
-                                    [title_text, link_text, getTimeFromStr(time_text), datetime.datetime.now(), '',
-                                     douban_url_name[douban_url_index], reply_count])
-                                print 'add new data:',title_text, time_text, reply_count,link_text
+                                    'INSERT INTO rent(id,title,url,itemtime,crawtime,author,source,keyword,note) VALUES(NULL,?,?,?,?,?,?,?,?)',
+                                    [title_text, link_text, getTimeFromStr(time_text), datetime.datetime.now(), '',keyword,
+                                     douban_url_name[douban_url_index],reply_count])
+                                print 'add new data:',title_text, time_text, reply_count,link_text,keyword
                             except sqlite3.Error, e:
                                 print 'data exists:',title_text,link_text,e
-                                pass
                     except Exception, e:
                         print "error match table", e
                 else:
-                    print 'request url error:' + url
+                    print 'request url error %s -status code: %s:' %(url_link,r.status_code)
+                time.sleep(1)
             douban_url_index+=1
     #end douban
 
@@ -201,7 +224,7 @@ try:
         file.writelines('<title>Rent Crawer Result</title></head><body>')
         file.writelines('<table rules=all>')
         file.writelines('<h1>' + prog_info + '</h1>')
-        file.writelines('<tr><td>索引Index</td><td>标题Title</td><td>链接Link</td><td>发帖时间Page Time</td><td>抓取时间Craw Time</td><td>作者Author</td><td>来源Source</td></tr>')
+        file.writelines('<tr><td>索引Index</td><td>标题Title</td><td>链接Link</td><td>发帖时间Page Time</td><td>抓取时间Crawl Time</td><td>作者Author</td><td>关键字Keyword</td><td>来源Source</td></tr>')
         for row in values:
             file.write('<tr>')
             for member in row:
@@ -222,4 +245,6 @@ except Exception, e:
 finally:
     conn.commit()
     conn.close()
-    print "Search Finish，Please open result.html to view result"
+    print "Search Finish,Please open result.html to view result"
+    #open result page
+    #webbrowser.open("result.html")
